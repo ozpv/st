9892de3813296a70451372856c31634f86ecd9f2
ozpv <39195175+ozpv@users.noreply.github.com>
2023-05-04 02:50:22 +0000

    change font

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "FiraCode Nerd Font Mono:style=Regular:pixelsize=16:antialias=true:autohint=true";
+static char *font = "JetBrainsMono Nerd Font:style=Regular:pixelsize=16:antialias=true:autohint=true";
 static int borderpx = 20;
 
 /*
